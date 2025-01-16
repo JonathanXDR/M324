4822bca2e64e152e5afc5b6872181027d5ad1af3
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2025-01-16 15:44:08 +0100

    feat: add http request

--- Code/src/routes/albums.ts
+++ Code/src/routes/albums.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import type { Band } from "@prisma/client";
 
 const router = Router();
+const axios = require('axios');
 
 router.get("/", (req, res) => {
   res.json({ message: "list of albums!" });
+});
+
+router.post("/", async (req, res) => {
+
+
+  const appBaseUrl = `${req.protocol}://${req.get('host')}`;
+  const bands: Array<Band> = await axios.get(`${appBaseUrl}/bands`);
+
+
+  res.json(response.data);
 });
 
 export default router;
