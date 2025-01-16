9c7ae13503572800a3306f7e7f53382b55c44c70
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2025-01-16 16:06:34 +0100

    feat: update create album endpoint

--- Code/src/routes/albums.ts
+++ Code/src/routes/albums.ts
@@ -1,5 +1,7 @@
+import { Label } from './../../node_modules/.prisma/client/index.d';
 import { Router } from "express";
-import type { Band } from "@prisma/client";
+import type { Album, Band } from "@prisma/client";
+import prisma from "../db/prisma";
 
 const router = Router();
 const axios = require('axios');
@@ -9,13 +11,46 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  const data = req.body;
 
+  /* Title */
+  const title = data.title;
 
+  /* Release Date */
+  const releaseDate = data.releaseDate;
+
+  /* Band */
+  const band: string = data.band;
   const appBaseUrl = `${req.protocol}://${req.get('host')}`;
-  const bands: Array<Band> = await axios.get(`${appBaseUrl}/bands`);
+  const response = await axios.get(`${appBaseUrl}/bands`);
+  const bands: Array<Band> = response.data;
+  const dbBand = bands.find((b) => b.name === band);
+  if (!dbBand) {
+    return res.status(400).json({ error: "Band not found" });
+  }
 
+  /* Price */
+  const price: number = data.price;
 
-  res.json(response.data);
+  /* Label */
+  const label = data.label;
+  const dbLabel = await prisma.band.findFirst({
+    where: {
+      name: label,
+    },
+  });
+
+  const newAlbum = await prisma.album.create({
+    data: {
+      title: title,
+      releaseDate: releaseDate,
+      price: price,
+      labelId: dbLabel!.id,
+      bandId: dbBand.id,
+    },
+  });
+
+  res.json(newAlbum);
 });
 
 export default router;
