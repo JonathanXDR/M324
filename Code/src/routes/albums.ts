50f68c1f02951c547f4ca46ac693eb2929d26e69
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-25 18:22:54 +0100

    feat(type): add return to albums

--- Code/src/routes/albums.ts
+++ Code/src/routes/albums.ts
@@ -1,46 +1,48 @@
-import type { Band } from '@prisma/client'
-import axios from 'axios'
-import type { Request, Response } from 'express'
-import { Router } from 'express'
-import prisma from '../db/prisma'
+import type { Band } from "@prisma/client";
+import axios from "axios";
+import type { Request, Response } from "express";
+import { Router } from "express";
+import prisma from "../db/prisma";
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'list of albums!' })
-})
+router.get("/", (req: Request, res: Response): void => {
+  res.json({ message: "list of albums!" });
+});
 
-router.post('/', async (req: Request, res: Response) => {
-  const data = req.body
+router.post("/", async (req: Request, res: Response): Promise<void> => {
+  const data = req.body;
 
   /* Title */
-  const title = data.title
+  const title = data.title;
 
   /* Release Date */
-  const releaseDate = new Date(data.releaseDate)
+  const releaseDate = new Date(data.releaseDate);
 
   /* Band */
-  const band: string = data.band
-  const appBaseUrl = `${req.protocol}://${req.get('host')}`
-  const response = await axios.get(`${appBaseUrl}/bands`)
-  const bands: Array<Band> = response.data
-  const dbBand = bands.find((b) => b.name === band)
+  const band: string = data.band;
+  const appBaseUrl = `${req.protocol}://${req.get("host")}`;
+  const response = await axios.get(`${appBaseUrl}/bands`);
+  const bands: Array<Band> = response.data;
+  const dbBand = bands.find((b) => b.name === band);
   if (!dbBand) {
-    return res.status(404).send()
+    res.status(404).send();
+    return;
   }
 
   /* Price */
-  const price: number = data.price
+  const price: number = data.price;
 
   /* Label */
-  const label = data.label
+  const label = data.label;
   const dbLabel = await prisma.label.findFirst({
     where: {
       name: label,
     },
-  })
+  });
   if (!dbLabel) {
-    return res.status(404).send()
+    res.status(404).send();
+    return;
   }
 
   const newAlbum = await prisma.album.create({
@@ -48,12 +50,12 @@ router.post('/', async (req: Request, res: Response) => {
       title,
       releaseDate,
       price,
-      labelId: dbLabel?.id,
-      bandId: dbBand?.id,
+      labelId: dbLabel.id,
+      bandId: dbBand.id,
     },
-  })
+  });
 
-  return res.json(newAlbum).send()
-})
+  res.json(newAlbum);
+});
 
-export default router
+export default router;
