bbc3aaed0eee487c453755a0153081fecdae7a88
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2025-01-26 16:35:24 +0100

    feat(validation): add validation

--- Code/src/routes/albums.ts
+++ Code/src/routes/albums.ts
@@ -16,6 +16,9 @@ router.post("/", async (req: Request, res: Response) => {
 
   /* Title */
   const title = data.title;
+  if (!title || title.length === 0) {
+    return res.status(400).send("Title is required");
+  }
 
   /* Release Date */
   const releaseDate = new Date(data.releaseDate);
@@ -27,11 +30,14 @@ router.post("/", async (req: Request, res: Response) => {
   const bands: Array<Band> = response.data.data;
   const dbBand = bands.find((b) => b.name === band);
   if (!dbBand) {
-    return res.status(404).send()
+    return res.status(404).send("Band not found");
   }
 
   /* Price */
   const price: number = data.price;
+  if (price < 0) {
+    return res.status(400).send("Price must be positive");
+  }
 
   /* Label */
   const label = data.label;
@@ -41,24 +47,31 @@ router.post("/", async (req: Request, res: Response) => {
     },
   });
   if (!dbLabel) {
-    return res.status(404).send();
+    return res.status(404).send("Label not found");
   }
 
-  const newAlbum = await prisma.album.create({
-    data: {
-      title: title,
-      releaseDate: releaseDate,
-      price: price,
-      labelId: dbLabel?.id,
-      bandId: dbBand?.id,
-    },
-    include: {
-      label: true,
-      band: true,
-    },
-  });
+  try {
+    const newAlbum = await prisma.album.create({
+      data: {
+        title: title,
+        releaseDate: releaseDate,
+        price: price,
+        labelId: dbLabel?.id,
+        bandId: dbBand?.id,
+      },
+      include: {
+        label: true,
+        band: true,
+      },
+    });
 
-  return res.json(newAlbum).send();
+    return res.json(newAlbum).send();
+  } catch (e: any) {
+    if (e instanceof Error) {
+      return res.status(500).send(e.message);
+    } else
+      return res.status(500).send("An error occurred while creating the album");
+  }
 });
 
 export default router;
