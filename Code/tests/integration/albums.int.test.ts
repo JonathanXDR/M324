c8adf8d1afc28bd760d11347be95264d611a58fc
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2025-01-28 18:59:07 +0100

    test: add itegration tests

---
+++ Code/tests/integration/albums.int.test.ts
@@ -0,0 +1,51 @@
+import { Server } from 'http'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import prisma from '../../src/db/prisma'
+import app from '../../src/index'
+
+let server: Server
+
+describe('Albums Integration Tests', () => {
+  beforeAll(async () => {
+    server = app.listen(4002)
+  })
+
+  afterAll(async () => {
+    server.close()
+    await prisma.$disconnect()
+  })
+
+  it('POST /albums should create a new album with valid data', async () => {
+    const res = await request(server).post('/albums').send({
+      title: 'Integration Album Test',
+      releaseDate: '2025-01-01',
+      band: 'The Beatles',
+      price: 30,
+      label: 'Sony Music',
+    })
+
+    expect(res.status).toBe(200)
+    expect(res.body.title).toBe('Integration Album Test')
+    expect(res.body.id).toBeTruthy()
+
+    const albumInDb = await prisma.album.findUnique({
+      where: { id: res.body.id },
+    })
+    expect(albumInDb?.title).toBe('Integration Album Test')
+
+    await prisma.album.delete({ where: { id: res.body.id } })
+  })
+
+  it('POST /albums should fail with 400 if "title" is missing', async () => {
+    const res = await request(server).post('/albums').send({
+      releaseDate: '2025-01-01',
+      band: 'The Beatles',
+      price: 30,
+      label: 'Sony Music',
+    })
+
+    expect(res.status).toBe(400)
+    expect(res.text).toBe('Title is required')
+  })
+})
---
+++ Code/tests/integration/bands.int.test.ts
@@ -0,0 +1,45 @@
+import { Server } from 'http'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import prisma from '../../src/db/prisma'
+import app from '../../src/index'
+
+let server: Server
+
+describe('Bands Integration Tests', () => {
+  beforeAll(async () => {
+    server = app.listen()
+  })
+
+  afterAll(async () => {
+    server.close()
+  })
+
+  it('GET /bands should return an array of bands', async () => {
+    const res = await request(server).get('/bands')
+    expect(res.status).toBe(200)
+    expect(res.body.success).toBe(true)
+    expect(Array.isArray(res.body.data)).toBe(true)
+  })
+
+  it('POST /bands should insert a band with associated albums', async () => {
+    const data = {
+      name: 'Integration Test Band',
+      foundingDate: '2025-01-01',
+      members: 3,
+      dissolutionDate: null,
+      genreId: 1,
+      albums: [{ title: 'Integration Album', price: 100, labelId: 1 }],
+    }
+    const res = await request(server).post('/bands').send(data)
+    expect(res.status).toBe(200)
+
+    expect(res.body).toMatchObject({
+      name: 'Integration Test Band',
+      albums: expect.any(Array),
+    })
+
+    const bandId = res.body.id
+    await prisma.band.delete({ where: { id: bandId } })
+  })
+})
