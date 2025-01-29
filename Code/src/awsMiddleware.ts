5f1f3b1bd3ef6098aff7be05d13e1ab38f71cecf
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2025-01-29 22:33:54 +0100

    chore: update awsMiddelware

--- Code/src/awsMiddleware.ts
+++ Code/src/awsMiddleware.ts
@@ -1,6 +1,45 @@
 import { type Request, type Response } from 'express'
 
+import AWS from 'aws-sdk'
+
+// Configure AWS credentials and region
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+})
+console.log('Updated aws config:')
+
+// Create a CloudWatch Logs client
+const cloudWatchLogs = new AWS.CloudWatchLogs()
+
+// Define the log group and stream names
+const logGroupName = process.env.AWS_LOG_GROUP_NAME || ''
+
+const logStreamName = process.env.AWS_LOG_STREAM_NAME || ''
+
+const logMessage = (message: string) => {
+  const params = {
+    logGroupName,
+    logStreamName,
+    logEvents: [
+      {
+        message,
+        timestamp: Date.now(),
+      },
+    ],
+  }
+
+  cloudWatchLogs.putLogEvents(params, (error: any, data: any) => {
+    if (error) {
+      console.log('Error writing log:', error)
+    } else {
+      console.log('Log written successfully:', data)
+    }
+  })
+}
 export const logRequest = (req: Request, res: Response, next: any) => {
-    console.log(`Request: ${req.method} ${req.url}`)
-    next()
+  logMessage('App started')
+  console.log(`Request: ${req.method} ${req.url}`)
+  next()
 }
--- Code/src/index.ts
+++ Code/src/index.ts
@@ -1,34 +1,15 @@
 import express, { type Request, type Response } from 'express'
 import albumRoute from './routes/albums.js'
 import bandRoute from './routes/bands.js'
-import { logRequest } from './awsMiddleware.ts'
+import { logRequest } from './awsMiddleware.js'
 
 const app = express()
 const port = process.env.APP_PORT || 3000
-const AWS = require('aws-sdk')
-
-// Configure AWS credentials and region
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-})
-
-// Create a CloudWatch Logs client
-const cloudWatchLogs = new AWS.CloudWatchLogs()
-
-// Define the log group and stream names
-const logGroupName = process.env.AWS_LOG_GROUP_NAME || ''
-
-const logStreamName = process.env.AWS_LOG_STREAM_NAME || ''
 
 app.use(logRequest)
 app.use(express.json())
 app.use('/bands', bandRoute)
 app.use('/albums', albumRoute)
-
-
-
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
@@ -37,29 +18,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
-
-const logMessage = (message: string) => {
-  const params = {
-    logGroupName: logGroupName,
-    logStreamName: logStreamName,
-    logEvents: [
-      {
-        message: message,
-        timestamp: Date.now()
-      }
-    ]
-  }
-
-  cloudWatchLogs.putLogEvents(params, (error: any, data: any) => {
-    if (error) {
-      console.log('Error writing log:', error)
-    } else {
-      console.log('Log written successfully:', data)
-    }
-  })
-}
-
-// Example usage: write a log message
-logMessage('App started')
 
 export default app
