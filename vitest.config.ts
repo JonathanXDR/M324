fad674c250e08713d86bf1cda6edb36710fe7371
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2025-01-30 13:52:12 +0100

    chore(config): remove default values

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'node',
     globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-    },
   },
 })
